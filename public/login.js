47e1db7993055148bd8ed77df5d06921db0c04a2
Geek2017 <jmlgrefaldo@bicol-u.edu.ph>
2022-06-13 19:49:57 +0800

    all

--- public/login.js
+++ public/login.js
@@ -1,12 +1,36 @@
 angular.module('cjfw').controller('loginCtrl', function($scope, $timeout) {
 
+    var config = {
+        apiKey: "AIzaSyCAG1P1ioOD5tdaxjPWcphdUyksk55uJ9k",
+        authDomain: "cjfwholesale.firebaseapp.com",
+        databaseURL: "https://cjfwholesale-default-rtdb.firebaseio.com/",
+        projectId: "cjfwholesale"
+    };
+
+    firebase.initializeApp(config);
+
+
     $scope.register = function() {
         window.location.href = "register.html"
     }
 
-    $('#loginForm').on('submit', function(e) {
-        e.preventDefault();
 
+    var Toast = Swal.mixin({
+        toast: true,
+        position: 'middle-center',
+        showConfirmButton: false,
+        timer: 3000
+    });
+
+    $(document).on('keypress', function(e) {
+        if (e.which == 13) {
+            $scope.signin();
+        }
+    });
+
+
+    $scope.signin = function() {
+        console.log('logging in..')
         firebase.database().ref('/users').orderByChild('email').equalTo($scope.email).on("value", function(snapshot) {
 
             snapshot.forEach(childSnapshot => {
@@ -20,13 +44,29 @@ angular.module('cjfw').controller('loginCtrl', function($scope, $timeout) {
 
                     console.log(pwd0, pwd1);
                     if (pwd0 == pwd1) {
-                        window.location.href = "index.html"
-                        sessionStorage.setItem('stat', 1)
+
+
+                        Toast.fire({
+                            icon: 'success',
+                            title: 'Authenticated'
+                        })
+
+                        setTimeout(() => {
+                            sessionStorage.setItem('stat', 1)
+
+                            window.location.href = "index.html"
+                        }, 1000);
+
+                    } else {
+                        Toast.fire({
+                            icon: 'error',
+                            title: ' Ivalid Entry Check Your Inputs'
+                        })
                     }
                 }
 
             });
         });
-    })
+    }
 
 });
--- public/register.js
+++ public/register.js
@@ -1,12 +1,28 @@
 angular.module('cjfw').controller('registerCtrl', function($scope, $timeout) {
+    var config = {
+        apiKey: "AIzaSyCAG1P1ioOD5tdaxjPWcphdUyksk55uJ9k",
+        authDomain: "cjfwholesale.firebaseapp.com",
+        databaseURL: "https://cjfwholesale-default-rtdb.firebaseio.com/",
+        projectId: "cjfwholesale"
+    };
+
+    firebase.initializeApp(config);
+
     $scope.backlogin = function() {
         window.location.href = "login.html"
     }
 
+    var Toast = Swal.mixin({
+        toast: true,
+        position: 'middle-center',
+        showConfirmButton: false,
+        timer: 3000
+    });
+
+
 
     $scope.registeruser = function() {
-
-
+        console.log(1)
 
         var uid = firebase.database().ref().child('/users').push().key;
 
@@ -19,18 +35,45 @@ angular.module('cjfw').controller('registerCtrl', function($scope, $timeout) {
 
         console.log(users)
 
-        var updates = {};
-        updates['/users/' + uid] = users;
-        firebase.database().ref().update(updates);
+        try {
+            var updates = {};
+            updates['/users/' + uid] = users;
+            firebase.database().ref().update(updates);
 
 
-        if (updates) {
-            console.log(updates)
-            alert('Success your regitered pls. login')
-            setTimeout(() => {
-                window.location.href = "login.html"
-            }, 2000);
+            if (updates) {
+                console.log(updates)
 
+                Toast.fire({
+                        icon: 'success',
+                        title: 'Register succesful, Kindly wait for admin to activate your account'
+                    })
+                    // setTimeout(() => {
+                    //     window.location.href = "login.html"
+                    // }, 2000);
+
+            }
+        } catch (error) {
+            Toast.fire({
+                icon: 'error',
+                title: error
+            })
         }
+
+
+        // firebase.database().ref('/users').orderByChild('email').equalTo(email).on("value", function(snapshot) {
+        //     var arr = [];
+
+        //     console.log(snapshot.val())
+
+        //     snapshot.forEach(childSnapshot => {
+        //         let item = childSnapshot.val();
+        //         item.key = childSnapshot.key;
+
+        //         arr.push(item[0]);
+        //     });
+        // });
+
+
     }
 });
