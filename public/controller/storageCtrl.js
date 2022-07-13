51084f21b502281feb34c5fb46049657d5f1f31f
Geek2017 <jmlgrefaldo@bicol-u.edu.ph>
2022-07-14 02:53:06 +0800

    4mat json dynamic div

--- public/controller/storageCtrl.js
+++ public/controller/storageCtrl.js
@@ -11,6 +11,52 @@ angular.module('cjfw').controller('storageCtrl', function($scope, $timeout) {
         timer: 4000
     });
 
+    var jsond=[];
+    let txtb=[];
+
+    $scope.showme=function(){
+
+        let drop=[];
+
+        $('#dtsdata .form-group').each(function() {
+            var d0 = $(this).find('input').val();
+            var d1 = $(this).find('option:selected').val();
+        
+           
+        
+
+            if(d0){
+                txtb.push(d0);
+            console.log('d0: ' +  d0);
+
+            }else{
+                if(d1){
+                    txtb.push(d1);
+                    console.log('d1: ' +  d1);
+                }
+               
+            }
+
+      });
+
+    //   $('#dtsdata .form-group  option:selected').each(function() {
+       
+    //     var dropdown = $(this).val();
+      
+    //     console.log('dropdown: ' + dropdown);
+
+     
+    //     if(dropdown){
+    //         drop.push(dropdown);
+    //     }
+    //      });
+
+      setTimeout(() => {
+        console.log(txtb)
+      }, 3000);
+    }
+
+
     $scope.tojson0 = function(obj0) {
         var table0 = $('#tblstorage0').tableToJSON({
             extractor: function(cellIndex, $cell) {
@@ -82,7 +128,7 @@ angular.module('cjfw').controller('storageCtrl', function($scope, $timeout) {
     let cnt0 = 0;
 
     $scope.addmaterial = function() {
-        $("#addmaterial0").append('<tr><td><strong class="badge badge-danger">' + parseInt(cnt0 + 2) + '</strong></td><td><input type="text" class="form-control" placeholder="PO #" required></td><td><input type="text" class="form-control" placeholder="Order #" required></td><td><input type="text" class="form-control" placeholder="Side Mark" required></td><td><input type="number" class="form-control val0" placeholder="Width" required></td><td><input type="number" class="form-control val1" placeholder="Length" required></td><td><input type="text" class="form-control qty" placeholder="Qty.Sqft" required></td><td><input type="text" class="form-control" placeholder="Roll #" required></td><td><select class="form-control" style="font-size:14px;"> <option value="0" selected disabled>SELECT</option> <option value="UPS">UPS</option> <option value="DHL">DHL</option> <option value="FedEx">FedEx</option> </select></td><td> <select class="form-control" ng-model="materialtype" style="font-size:14px;"> <option disabled value="">SELECT</option> <option value="Acessories">Acessories</option> <option value="Area Rugs">Area Rugs</option> <option value="Cabinet">Cabinet</option> <option value="Carpet">Carpet</option> <option value="Carpet Tile">Carpet Tile</option> <option value="Ceilings">Ceilings</option> <option value="Ceramic Tile">Ceramic Tile</option> <option value="Displays">Displays</option> <option value="Fixtures">Fixtures</option> <option value="Installation Materials">Installation Materials</option> <option value="Laminates">Laminates</option> <option value="Pad">Pad</option> <option value="Rubber Tile">Rubber Tile</option> <option value="Runner">Runner</option> <option value="Stone">Stone</option> <option value="Training">Training</option> <option value="Unclassified">Unclassified</option> <option value="Vinyl">Vinyl</option> <option value="Vinyl Sheet">Vinyl Sheet</option> <option value="Vinyl Tile">Vinyl Tile</option> <option value="Wall Base">Wall Base</option> <option value="Wall Coverings">Wall Coverings</option> <option value="wood">wood</option> </select> </td><td> <select class="form-control" ng-model="store"> <option disabled value="" style="font-size:14px;">SELECT</option> <option value="Carpet Yard">Carpet Yard</option> <option value="EB-Carpets &amp; More East Brunswick">EB-Carpets &amp; More East Brunswick</option> <option value="Warehouse">Warehouse</option> <option value="SI-Carpets &amp; More Staten Island">SI-Carpets &amp; More Staten Island</option> <option value="OB-Carpets &amp; More Old Bridge">OB-Carpets &amp; More Old Bridge</option> </select> </td><td> <input type="text" class="form-control" ng-model="description" placeholder="Description"> </td></tr>');
+        $("#dtsdata").append(' <div class="card" > <div class="card-header"> <h3 class="card-title">Details:</h3> <div class="card-tools"> <button type="button" class="btn btn-tool" data-card-widget="collapse"> <i class="fas fa-minus"></i> </button> <button type="button" class="btn btn-tool" data-card-widget="remove"> <i class="fas fa-times"></i> </button> </div></div><div class="card-body"> <div class="row"> <div class="col-md-2"> <div class="form-group"> <label>PO No</label> <input type="text" name="txtd" class="form-control" placeholder="Enter ..." > </div></div><div class="col-md-2"> <div class="form-group"> <label>Order No</label> <input type="text" name="txtd" class="form-control" placeholder="Enter ..." > </div></div><div class="col-md-2"> <div class="form-group"> <label>SideMark</label> <input type="text" name="txtd" class="form-control" placeholder="Enter ..." > </div></div><div class="col-md-2"> <div class="form-group"> <label>Width</label> <input type="text" name="txtd" class="form-control" placeholder="Enter ..." > </div></div><div class="col-md-2"> <div class="form-group"> <label>Length</label> <input type="text" name="txtd" class="form-control" placeholder="Enter ..." > </div></div><div class="col-md-2"> <div class="form-group"> <label>Qty SqFt</label> <input type="text" name="txtd" class="form-control" placeholder="Enter ..." > </div></div><div class="col-md-2"> <div class="form-group"> <label>Roll No</label> <input type="text" name="txtd" class="form-control" placeholder="Enter ..." > </div></div><div class="col-md-2"> <div class="form-group"> <label>Store</label> <select name="txtd" class="form-control" style="font-size:14px;"> <option disabled value="" >SELECT</option> <option value="Carpet Yard">Carpet Yard</option> <option value="EB-Carpets &amp; More East Brunswick">EB-Carpets &amp; More East Brunswick</option> <option value="Warehouse">Warehouse</option> <option value="SI-Carpets &amp; More Staten Island">SI-Carpets &amp; More Staten Island</option> <option value="OB-Carpets &amp; More Old Bridge">OB-Carpets &amp; More Old Bridge</option> </select> </div></div><div class="col-md-2"> <div class="form-group"> <label>Material Type</label> <select name="txtd" class="form-control" ng-model="materialtype" style="font-size:14px;"> <option disabled value="">SELECT</option> <option value="Acessories">Acessories</option> <option value="Area Rugs">Area Rugs</option> <option value="Cabinet">Cabinet</option> <option value="Carpet">Carpet</option> <option value="Carpet Tile">Carpet Tile</option> <option value="Ceilings">Ceilings</option> <option value="Ceramic Tile">Ceramic Tile</option> <option value="Displays">Displays</option> <option value="Fixtures">Fixtures</option> <option value="Installation Materials">Installation Materials</option> <option value="Laminates">Laminates</option> <option value="Pad">Pad</option> <option value="Rubber Tile">Rubber Tile</option> <option value="Runner">Runner</option> <option value="Stone">Stone</option> <option value="Training">Training</option> <option value="Unclassified">Unclassified</option> <option value="Vinyl">Vinyl</option> <option value="Vinyl Sheet">Vinyl Sheet</option> <option value="Vinyl Tile">Vinyl Tile</option> <option value="Wall Base">Wall Base</option> <option value="Wall Coverings">Wall Coverings</option> <option value="wood">wood</option> </select> </div></div><div class="col-md-2"> <div class="form-group"> <label>Carrier</label> <select name="txtd" class="form-control" style="font-size:14px;"> <option value="0" selected disabled>SELECT</option> <option value="UPS">UPS</option> <option value="DHL">DHL</option> <option value="FedEx">FedEx</option> </select> </div></div><div class="col-md-4"> <div class="form-group"> <label>Description</label> <input type="text" name="details" class="form-control" placeholder="Enter ..." > </div></div></div></div></div>');
 
 
         cnt0++;
@@ -93,7 +139,8 @@ angular.module('cjfw').controller('storageCtrl', function($scope, $timeout) {
         console.log(cnt0)
         if (cnt0 !== 0) {
 
-            $('#addmaterial0 tr:last').remove();
+
+            $("#adtsdata").children("div[id=append]:last").fadeOut();
 
             cnt0--;
         }
@@ -111,85 +158,87 @@ angular.module('cjfw').controller('storageCtrl', function($scope, $timeout) {
     });
 
 
+
+
     $scope.savematerial = function() {
 
-        const mdata = angular.merge($scope.tojson0(obj0));
+    const mdata = angular.merge($scope.tojson0(obj0));
 
         console.log(mdata);
 
 
-        var uid = firebase.database().ref().child('/storage').push().key;
+        // var uid = firebase.database().ref().child('/storage').push().key;
 
-        var storage = {
-            timstamp: firebase.database.ServerValue.TIMESTAMP,
-            date: $scope.curdate,
-            billofland: $scope.billofland,
-            vendor: $scope.vendor,
-            details: mdata,
-        }
-
-
-        $scope.ladings = mdata;
-
-        console.log(storage);
-
-        $scope.recvdate = $scope.curdate
-        $('#modal-xl').modal('toggle');
-
-        var updates = {};
-        updates['/storage/' + uid] = storage;
-        firebase.database().ref().update(updates);
-
-        let repeat = mdata.length;
-
-        console.log(repeat)
-
-        console.log(mdata);
-
-        if (updates) {
-            console.log(updates)
-            setTimeout(() => {
-                if (repeat) {
-
-                    var cntr = '#container';
-                    var bids = 'barcode';
-
-                    angular.forEach(mdata, function(value, key) {
-                        console.log(key + 1)
-
-                        let nc = cntr + parseInt(key + 1);
-
-                        var ids = bids + key;
-
-                        console.log(nc, ids);
-
-                        $(nc).append($("<div id='" + ids + "'></div>"));
-
-                        setTimeout(() => {
-                            $('#' + ids).barcode(
-                                mdata[key]['Roll No'],
-                                "code39", {
-                                    barWidth: 4,
-                                    barHeight: 100,
-                                    fontSize: 14
-                                }
-                            );
-
-                            console.log(mdata[key]['Roll No']);
-                        }, 500);
+        // var storage = {
+        //     timstamp: firebase.database.ServerValue.TIMESTAMP,
+        //     date: $scope.curdate,
+        //     billofland: $scope.billofland,
+        //     vendor: $scope.vendor,
+        //     details: mdata,
+        // }
 
 
-                    });
-                }
-            }, 500);
-        }
+        // $scope.ladings = mdata;
 
-        $scope.printhis = function() {
-            $('#pmodal').kinziPrint({
-                importCSS: true,
-                loadCSS: 'dist/css/print.css'
-            });
-        }
+        // console.log(storage);
+
+        // $scope.recvdate = $scope.curdate
+        // $('#modal-xl').modal('toggle');
+
+        // var updates = {};
+        // updates['/storage/' + uid] = storage;
+        // firebase.database().ref().update(updates);
+
+        // let repeat = mdata.length;
+
+        // console.log(repeat)
+
+        // console.log(mdata);
+
+        // if (updates) {
+        //     console.log(updates)
+        //     setTimeout(() => {
+        //         if (repeat) {
+
+        //             var cntr = '#container';
+        //             var bids = 'barcode';
+
+        //             angular.forEach(mdata, function(value, key) {
+        //                 console.log(key + 1)
+
+        //                 let nc = cntr + parseInt(key + 1);
+
+        //                 var ids = bids + key;
+
+        //                 console.log(nc, ids);
+
+        //                 $(nc).append($("<div id='" + ids + "'></div>"));
+
+        //                 setTimeout(() => {
+        //                     $('#' + ids).barcode(
+        //                         mdata[key]['Roll No'],
+        //                         "code39", {
+        //                             barWidth: 4,
+        //                             barHeight: 100,
+        //                             fontSize: 14
+        //                         }
+        //                     );
+
+        //                     console.log(mdata[key]['Roll No']);
+        //                 }, 500);
+
+
+        //             });
+        //         }
+        //     }, 500);
+        // }
+
+        // $scope.printhis = function() {
+        //     $('#pmodal').kinziPrint({
+        //         importCSS: true,
+        //         loadCSS: 'dist/css/print.css'
+        //     });
+        // }
     }
 
 
